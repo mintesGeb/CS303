360a26d434d6245fab8a2506719737f88a0d8acd
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-03-18 19:31:46 -0400

    update

--- labW1D1/app.js
+++ labW1D1/app.js
@@ -3,21 +3,21 @@
 "use strict";
 
 function pow(x, n) {
-	return x * n;
+  return x ** n;
 }
 
 function add2(n1, n2) {
-	return n1 + n2;
+  return n1 + n2;
 }
 
 function add(...args) {
-	let count = arguments.length;
-	let sum = 0;
-	for (let i=0; i<count; i++) {
-		sum += arguments[i];		
-	}
-	
-	return sum;
+  let count = arguments.length;
+  let sum = 0;
+  for (let i = 0; i < count; i++) {
+    sum += arguments[i];
+  }
+
+  return sum;
 }
 
 /*
@@ -30,6 +30,5 @@ function add(...args) {
 	0 - 59   NC
 */
 function computeGrade(grades) {
-	return "???";
+  return "???";
 }
-
