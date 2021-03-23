5d98df24ce77b7cd493ea05688bef4a84d296e25
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-03-23 13:02:12 -0400

    w2d2

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -1,4 +1,24 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D2";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+// See tests.js for behavior
+function nameString(obj) {
+  let sum = "";
+  if (obj === null) return `${obj} argument`;
+
+  let keys = Object.keys(obj);
+  if (keys.length === 0) return `object has no properties`;
+  if (keys.length === 0) {
+    return `no elements`;
+  } else {
+    for (let each of keys) {
+      sum += each + ".";
+    }
+    sum = sum.slice(0, -1);
+  }
+  return sum;
 }
+
+let x = nameString({});
+console.log(x);
