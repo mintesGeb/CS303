7a817fbb97afd4bb1d86c24aa17c8216c8e28cff
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-03-24 12:09:08 -0400

    w1d3

--- labW1D3/app.js
+++ labW1D3/app.js
@@ -1,4 +1,48 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D3";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+/*
+Function analyzer
+Has an unbound this
+Creates an object that contains the following information from the "this" object. 
+{
+	numProperties  	// number of properties
+	cntShortName	// count of property names shorter than 3 characters
+	cntReferences	// count of reference properties (use the typeof operator to compute this)
+}	
+*/
+function analyzer() {
+  return {};
 }
+
+/* Constructor for a person object
+	Person(name, country, grades) creates object
+	{
+		name: name,  // person's name
+		county: country, // person's country
+		grades: grades,  // integer array of grades
+		computeGrade     // function that computes the grade from grades
+	}
+*/
+function Person(name, country, grades) {
+  this.name = name;
+  this.country = country;
+  this.grades = grades;
+  this.computeGrade = function () {
+    let sum = 0;
+    let arr = this.grades;
+    for (let each of arr) {
+      sum += each;
+    }
+    let ave = sum / arr.length;
+    return ave;
+  };
+  //   this.analyzer=function (){
+  // 	  this.
+  //   }
+}
+
+// let x = new Person("mintes", "Ethio", [90, 100]);
+// console.log(x);
+// console.log(x.computeGrade());
